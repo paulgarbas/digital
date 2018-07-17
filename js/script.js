183de9655aa7dd1b38500d6cc44aeba77dd5ba4f
Paulius <garbasauskas@gmail.com>
2018-07-17 18:18:37 +0300

    Dynamic background, animated hamburger

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
 "use strict";
 
-let squareRotates = document.querySelector(".square");
-let squareMovesLeft = document.querySelector(".left-move");
+let square = document.querySelector(".square");
+let textWrapper = document.querySelector(".text__wrapper");
 let text = document.querySelector(".text");
 let squareRightBorder = document.querySelector(".square-border-right");
+let logo = document.querySelector(".logo");
+let canvas = document.querySelector("canvas");
+let hamburger = document.querySelector(".hamburger");
 
 let executed = false;
 
-squareRotates.addEventListener("animationend", squareRotationEnd);
+square.addEventListener("animationend", squareRotationEnd);
+hamburger.addEventListener("click", addActiveClass);
 
 function squareRotationEnd() {
     if (!executed) { 
-        squareRotates.className += " left-move";
-        squareRotates.addEventListener("animationend", squareMovingEnd);
+        squareRightBorder.className += " pushed-border";
+        text.className += " right-move";
+        textWrapper.className += " left-move-text__wrapper";  
+        square.className += " left-move-square";      
+        square.addEventListener("animationend", collapsingBorder);
     }
     executed = true;
 }
 
-function squareMovingEnd() {
-    squareRotates.style.borderRight = "none";
-    text.className += " right-move";
-    squareRightBorder.className += " pushed-border";
-    squareRightBorder.addEventListener("animationend", collapsingBorder);
-}
-
 function collapsingBorder() {
-    squareRightBorder.className += " collapsed-border";     
+    squareRightBorder.className += " collapsed-border"; 
+    squareRightBorder.addEventListener("animationend", textMovingDown)
 }
 
+function textMovingDown() {
+    square.classList.remove("left-move-square");
+    square.className += " down-move-square";        
+    textWrapper.className += " down-move-text__wrapper";
+    square.addEventListener("animationend", fading)  ;  
+}
+
+function fading() { 
+    canvas.className += " body-bg";
+    logo.className += " fade-in-logo";
+    square.className += " fade-out fade-out-box";
+    squareRightBorder.className += " fade-out fade-out-right-border";     
+}
+
+function addActiveClass() {
+    hamburger.classList.toggle("is-active");
+}
