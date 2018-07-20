20020c6779674e6ea79d672bd9425f091b42ae5b
Paulius <garbasauskas@gmail.com>
2018-07-20 17:06:05 +0300

    small logo in left upper corner, bouncing arrow, a little bit changed main logo animation

--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ let text = document.querySelector(".text");
 let squareRightBorder = document.querySelector(".square-border-right");
 let logo = document.querySelector(".logo");
 let canvas = document.querySelector("canvas");
-let executed = false;
+let executedSquareRotation = false;
+let executedCollapsingBorder = false;
+let executedLittleBox = false;
+let executedTextMoving = false;
+let executedBigLogo = false;
+
+// Small logo
+let smallLogo = document.querySelector(".small-logo"); 
+let smallLogoImg = document.querySelector(".small-logo-img");
 
 // Major navigation
 let navMajor = document.querySelector(".navigation-major");
@@ -24,41 +32,64 @@ let navWeb = document.querySelector(".nav-web");
 let navApp = document.querySelector(".nav-app");
 let navBusiness = document.querySelector(".nav-business");
 
+// Bouncing arrow
+let bouncingArrow = document.querySelector(".arrow-bouncing"); 
+let bouncingArrowLink = document.querySelector(".arrow-bouncing-link");
+
 square.addEventListener("animationend", squareRotationEnd);
 hamburger.addEventListener("click", addActiveClass); 
 
 function squareRotationEnd() {
-    if (!executed) { 
+    if (!executedSquareRotation) { 
         squareRightBorder.className += " pushed-border";
         text.className += " right-move";
         textWrapper.className += " left-move-text__wrapper";  
+        textWrapper.style.right = "-130px";
         square.className += " left-move-square";      
         square.addEventListener("animationend", collapsingBorder);
     }
-    executed = true;
+    executedSquareRotation = true;
 }
 
 function collapsingBorder() {
-    squareRightBorder.className += " collapsed-border"; 
-    squareRightBorder.addEventListener("animationend", textMovingDown)
+    if (!executedCollapsingBorder) { 
+        squareRightBorder.className += " collapsed-border"; 
+        squareRightBorder.addEventListener("animationend", littleBoxFadeOut);
+    }
+    executedCollapsingBorder = true;    
+}
+
+function littleBoxFadeOut() {
+    if (!executedLittleBox) {         
+        square.className += " fade-out fade-out-box";
+        squareRightBorder.className += " fade-out fade-out-right-border";   
+        square.addEventListener("animationend", textMovingDown);
+    }
+    executedLittleBox = true;    
 }
 
 function textMovingDown() {
-    square.classList.remove("left-move-square");
-    square.className += " down-move-square";        
-    textWrapper.className += " down-move-text__wrapper";
-    square.addEventListener("animationend", fading);  
+    if (!executedTextMoving) {         
+        // square.classList.remove("left-move-square");
+        // square.className += " down-move-square";        
+        textWrapper.className += " down-move-text__wrapper";
+        textWrapper.addEventListener("animationend", bigLogoFadeIn);  
+    }
+    executedTextMoving = true;        
 }
 
-function fading() { 
-    canvas.className += " body-bg";
-    logo.className += " fade-in-logo";
-    square.className += " fade-out fade-out-box";
-    squareRightBorder.className += " fade-out fade-out-right-border";   
-    square.addEventListener("animationend", addNavigation);      
+function bigLogoFadeIn() { 
+    if (!executedBigLogo) {                 
+        canvas.className += " body-bg";
+        logo.className += " fade-in-logo";
+        logo.addEventListener("animationend", addNavigation);  
+    }   
+    executedBigLogo = true;        
 }
 
 function addNavigation() {
+    smallLogo.style.display = "block";
+    smallLogoImg.className += " small-logo-animation"; 
     navMajor.style.display = "flex";
     navMinor.style.display = "flex";
     navHome.className += " nav-home-animation";
@@ -67,7 +98,8 @@ function addNavigation() {
     navTestimonials.className += " nav-testimonials-animation";
     navContacts.className += " nav-contacts-animation";
     navHamburger.className += " nav-hamburger-animation";
-    
+    bouncingArrow.style.display = "inline-block";
+    bouncingArrowLink.className += " arrow-bouncing-link-animation";
 }
 
 function addActiveClass() {
